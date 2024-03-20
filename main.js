33c41b20faf1b2f4040b0dc92eff0e75ebd10746
Utkarshkusumwal <135632104+Utkarshkusumwal@users.noreply.github.com>
2024-03-20 19:52:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+diffrence = 10;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(400, 400);
+    canvas.position(520, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized!");
+}
+
+function draw() {
+    background("white");
+    textSize(diffrence);
+    fill("Royalblue");
+    text("Code", 200, 250);
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
+    rightWrist = results[0].pose.rightWrist.x;
+    leftWrist = results[0].pose.leftWrist.x;
+
+    diffrence = floor(leftWrist - rightWrist);
+ 
+    console.log("leftWristX = " + leftWrist + " rightWristX = " + rightWrist + " diffrence = " + diffrence);
+}
